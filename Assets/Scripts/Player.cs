8007a09a61b6be7c98a383a0727820c4b5e9b1d1
Vovun <mrvovun@gmail.com>
2018-06-06 10:59:19 +0200

    Initial + controller
    Initial commit with player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        float veticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * Time.deltaTime * speed * veticalInput);
+
+        if (transform.position.y > 4.3f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.3f, 0);
+        }
+        else if (transform.position.y < -4.3f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.3f, 0);
+        }
+
+        if (transform.position.x > 8.2f)
+        {
+            transform.position = new Vector3(8.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8.2f)
+        {
+            transform.position = new Vector3(-8.2f, transform.position.y, 0);
+        }
+    }
+
+}
